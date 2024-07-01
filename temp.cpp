e351bd1cd2c87bf26efbdaa8ff4848587370f143
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-07-01 15:19:02 +0100

    program updates

--- temp.cpp
+++ temp.cpp
@@ -2,13 +2,17 @@
 
 int main() {
   
-  double tempf = 83.0;
-
+  double tempf;
   double tempc;
+  
+  // Ask the user
+  
+  std::cout << "Enter the temperature in Fahrenheit: ";
+
+  std::cin >> tempf;
   
   tempc = (tempf - 32) / 1.8;
   
   std::cout << "The temp is " << tempc << " degrees Celsius.\n";
-  
   
 }
