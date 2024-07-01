994b82dc8009246e9b25d7d4becf9ca3f467f182
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-07-01 15:01:27 +0100

    Finished project

---
+++ initials.cpp
@@ -0,0 +1,15 @@
+// Fun Fact: I can speak Mandarin, Spanish, Korean, German and a little bit of Japanese
+
+#include <iostream>
+
+int main() {
+
+  std::cout << " OOO   N   N  EEEEE\n";
+  std::cout << "O   O  NN  N  E    \n";
+  std::cout << "O   O  N N N  E    \n";
+  std::cout << "O   O  N  NN  EEE  \n";
+  std::cout << "O   O  N   N  E    \n";
+  std::cout << "O   O  N   N  E    \n";
+  std::cout << " OOO   N   N  EEEEE\n";
+
+}
